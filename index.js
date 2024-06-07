17f0b8c23204696aec5159bcfffde5137ede426b
Sangram Bishwas <info.sangram2003@gmail.com>
2024-06-08 02:48:38 +0600

    another get operation for reviews api

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const classCollection = client.db("goldGymDB").collection("classes");
+        const reviewsCollection = client.db("goldGymDB").collection("reviews");
+
+        app.get('/featured&classes', async (req, res) => {
+            const result = await classCollection.find().sort({ number_of_bookings: -1 }).toArray();
+            res.send(result.slice(0, 6))
+        })
+        app.get('/reviews', async(req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
