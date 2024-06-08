979a8d6cc9a9851e3d8d98b727c0fd0b9f14b60b
Sangram Bishwas <info.sangram2003@gmail.com>
2024-06-09 01:45:58 +0600

    make anothere get operation for getPost

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mmbmgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 //Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mmbmgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,34 +21,38 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
+async function dbConnect() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        const classCollection = client.db("goldGymDB").collection("classes");
-        const reviewsCollection = client.db("goldGymDB").collection("reviews");
-
-        app.get('/featured&classes', async (req, res) => {
-            const result = await classCollection.find().sort({ number_of_bookings: -1 }).toArray();
-            res.send(result.slice(0, 6))
-        })
-        app.get('/reviews', async(req, res) => {
-            const result = await reviewsCollection.find().toArray();
-            res.send(result);
-        })
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+        // await client.db('admin').command({ ping: 1 })
+        console.log('You successfully connected to MongoDB!')
+    } catch (err) {
+        console.log(err)
     }
 }
-run().catch(console.dir);
-
+dbConnect()
+const classCollection = client.db("goldGymDB").collection("classes");
+const reviewsCollection = client.db("goldGymDB").collection("reviews");
+const postsCollection = client.db("goldGymDB").collection("posts");
 
 app.get('/', (req, res) => {
     res.send('Hello GoldGYM!')
+})
+
+app.get('/featured&classes', async (req, res) => {
+    const result = await classCollection.find().sort({ number_of_bookings: -1 }).toArray();
+    res.send(result.slice(0, 6))
+})
+
+//Review
+app.get('/reviews', async (req, res) => {
+    const result = await reviewsCollection.find().toArray();
+    res.send(result);
+})
+
+//Community posts
+app.get('/getPost', async (req, res) => {
+    const result = (await postsCollection.find().toArray()).reverse();
+    res.send(result.slice(0, 6))
 })
 
 app.listen(port, () => {
